3be22d543a1eb55b5b3d1bc27035ed3ac021c6f3
Ranko Radonić <ranko.radonic@gmx.com>
2016-02-15 13:58:41 +0100

    output all page titles

--- sax2-handler.cpp
+++ sax2-handler.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 #include "sax2-handler.hpp"
 
@@ -8,7 +9,49 @@ void SAX2Handler::startElement(
     const XMLCh* const qname,
     const xercesc::Attributes& attrs) {
 
-    char* message = xercesc::XMLString::transcode(localname);
-    std::cout << "I saw element: "<< message << std::endl;
-    xercesc::XMLString::release(&message);
+    auto deleter = [](char* p) {
+        xercesc::XMLString::release(&p);
+    };
+
+    std::unique_ptr<char, decltype(deleter)> nodeName(
+        xercesc::XMLString::transcode(localname),
+        deleter);
+
+    this->state.push(std::string(nodeName.get()));
+}
+
+void SAX2Handler::endElement(
+    const XMLCh* const uri,
+    const XMLCh* const localname,
+    const XMLCh* const qname) {
+
+    auto deleter = [](char* p) {
+        xercesc::XMLString::release(&p);
+    };
+
+    std::unique_ptr<char, decltype(deleter)> nodeName(
+        xercesc::XMLString::transcode(localname),
+        deleter);
+
+    if(this->state.top() == "title") {
+        std::cout << this->text << std::endl;
+    }
+
+    this->state.pop();
+    this->text.clear();
+}
+
+void SAX2Handler::characters(
+    const XMLCh* const chars,
+    const XMLSize_t length) {
+
+    if(this->state.top() != "title") {
+        return;
+    }
+
+    std::unique_ptr<char[]> buffer(new char[length + 1]);
+
+    xercesc::XMLString::transcode(chars, buffer.get(), length);
+
+    this->text.append(buffer.get());
 }
--- sax2-handler.hpp
+++ sax2-handler.hpp
@@ -1,3 +1,5 @@
+#include <stack>
+
 #include <xercesc/sax2/DefaultHandler.hpp>
 
 class SAX2Handler : public xercesc::DefaultHandler {
@@ -6,6 +8,17 @@ public:
         const XMLCh* const uri,
         const XMLCh* const localname,
         const XMLCh* const qname,
-        const xercesc::Attributes& attrs
-    );
+        const xercesc::Attributes& attrs);
+
+    void endElement(
+        const XMLCh* const uri,
+        const XMLCh* const localname,
+        const XMLCh* const qname);
+
+    void characters(const XMLCh* const chars,
+        const XMLSize_t length);
+
+private:
+    std::stack<std::string> state;
+    std::string text;
 };
