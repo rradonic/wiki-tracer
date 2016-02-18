bf6e13fd0f1be019e3e46074d0552641c293ff34
Ranko Radonić <ranko.radonic@gmx.com>
2016-02-18 22:56:25 +0100

    simplified link filtering
    we'll filter them further in the next phase, where we check for broken
    links

--- graph-extractor/sax2-handler.cpp
+++ graph-extractor/sax2-handler.cpp
@@ -18,22 +18,6 @@ namespace std {
     };
 }
 
-const char* SAX2Handler::languages[] = {
-    "aa:", "ab:", "ae:", "af:", "ak:", "am:", "an:", "ar:", "as:", "av:", "ay:", "az:", "ba:", "be:", "bg:",
-    "bh:", "bi:", "bm:", "bn:", "bo:", "br:", "bs:", "ca:", "ce:", "ch:", "co:", "cr:", "cs:", "cu:", "cv:",
-    "cy:", "da:", "de:", "dv:", "dz:", "ee:", "el:", "en:", "eo:", "es:", "et:", "eu:", "fa:", "ff:", "fi:",
-    "fj:", "fo:", "fr:", "fy:", "ga:", "gd:", "gl:", "gn:", "gu:", "gv:", "ha:", "he:", "hi:", "ho:", "hr:",
-    "ht:", "hu:", "hy:", "hz:", "ia:", "id:", "ie:", "ig:", "ii:", "ik:", "io:", "is:", "it:", "iu:", "ja:",
-    "jv:", "ka:", "kg:", "ki:", "kj:", "kk:", "kl:", "km:", "kn:", "ko:", "kr:", "ks:", "ku:", "kv:", "kw:",
-    "ky:", "la:", "lb:", "lg:", "li:", "ln:", "lo:", "lt:", "lu:", "lv:", "mg:", "mh:", "mi:", "mk:", "ml:",
-    "mn:", "mr:", "ms:", "mt:", "my:", "na:", "nb:", "nd:", "ne:", "ng:", "nl:", "nn:", "no:", "nr:", "nv:",
-    "ny:", "oc:", "oj:", "om:", "or:", "os:", "pa:", "pi:", "pl:", "ps:", "pt:", "qu:", "rm:", "rn:", "ro:",
-    "ru:", "rw:", "sa:", "sc:", "sd:", "se:", "sg:", "si:", "sk:", "sl:", "sm:", "sn:", "so:", "sq:", "sr:",
-    "ss:", "st:", "su:", "sv:", "sw:", "ta:", "te:", "tg:", "th:", "ti:", "tk:", "tl:", "tn:", "to:", "tr:",
-    "ts:", "tt:", "tw:", "ty:", "ug:", "uk:", "ur:", "uz:", "ve:", "vi:", "vo:", "wa:", "wo:", "xh:", "yi:",
-    "yo:", "za:", "zh:", "zu:"
-};
-
 SAX2Handler::SAX2Handler() : redirect(false) {
 }
 
@@ -65,10 +49,7 @@ void SAX2Handler::endElement(
     (void)localname;
     (void)qname;
 
-    if(this->state.top() == "page" &&
-        !this->redirect &&
-        this->allowedTitle(this->title)) {
-
+    if(this->state.top() == "page" && !this->redirect && this->allowed(this->title)) {
         std::unordered_set<icu::UnicodeString> links;
 
         UErrorCode status = U_ZERO_ERROR;
@@ -80,8 +61,9 @@ void SAX2Handler::endElement(
 
         while(matcher.find()) {
             icu::UnicodeString link = matcher.group(1, status);
+            link.toLower();
 
-            if(this->allowedLink(link)) {
+            if(this->allowed(link)) {
                 links.insert(link);
             }
         }
@@ -89,10 +71,7 @@ void SAX2Handler::endElement(
         std::cout << this->title << ": ";
 
         for(auto const& link : links) {
-            icu::UnicodeString linkCapitalized(link[0]);
-            linkCapitalized.toUpper();
-
-            std::cout << " [[" << linkCapitalized.append(link, 1, link.length() - 1) << "]]";
+            std::cout << " [[" << link << "]]";
         }
 
         std::cout << std::endl;
@@ -118,81 +97,31 @@ void SAX2Handler::characters(
     icu::UnicodeString charsUnicode(chars, length);
 
     if(this->state.top() == "title") {
+        charsUnicode.toLower();
         this->title.append(charsUnicode);
     } else if(this->state.top() == "text") {
         this->content.append(charsUnicode);
     }
 }
 
-bool SAX2Handler::allowedTitle(icu::UnicodeString title) const {
-    icu::UnicodeString titleLower(title);
-    titleLower.toLower();
-
-    return this->allowed(titleLower);
-}
-
-bool SAX2Handler::allowedLink(icu::UnicodeString link) const {
-    icu::UnicodeString linkLower(link);
-    linkLower.toLower();
-
-    return
-        this->allowed(linkLower) &&
-        !this->interLanguage(linkLower) &&
-
-        linkLower.compare(0, 2, "m:") != 0 &&
-        linkLower.compare(0, 3, "mw:") != 0 &&
-        linkLower.compare(0, 5, "wikt:") != 0 &&
-        linkLower.compare(0, 11, "wiktionary:") != 0 &&
-        linkLower.compare(0, 2, "s:") != 0 &&
-        linkLower.compare(0, 11, "wikisource:") != 0 &&
-        linkLower.compare(0, 2, "q:") != 0 &&
-        linkLower.compare(0, 10, "wikiquote:") != 0 &&
-        linkLower.compare(0, 4, "doi:") != 0 &&
-        linkLower.compare(0, 4, "hdl:") != 0 &&
-        linkLower.compare(0, 8, "species:") != 0 &&
-        linkLower.compare(0, 2, "v:") != 0 &&
-        linkLower.compare(0, 5, "talk:") != 0 &&
-        linkLower.compare(0, 4, "ikt:") != 0 &&
-        linkLower.compare(0, 10, "iktionary:") != 0 &&
-        linkLower.compare(0, 8, "commons:") != 0 &&
-        linkLower.compare(0, 10, "ikisource:") != 0 &&
-        linkLower.compare(0, 2, "b:") != 0 &&
-        linkLower.compare(0, 4, "mos:") != 0;
-}
-
 bool SAX2Handler::allowed(icu::UnicodeString text) const {
     return
         text.compare(0, 10, "wikipedia:") != 0 &&
-        text.compare(0, 8, "project:") != 0 &&
         text.compare(0, 3, "wp:") != 0 &&
+        text.compare(0, 8, "project:") != 0 &&
 
         text.compare(0, 5, "file:") != 0 &&
         text.compare(0, 6, "image:") != 0 &&
         text.compare(0, 6, "media:") != 0 &&
 
-        text.compare(0, 5, "user:") != 0 &&
-        text.compare(0, 10, "mediaWiki:") != 0 &&
+        text.compare(0, 10, "mediawiki:") != 0 &&
         text.compare(0, 9, "template:") != 0 &&
         text.compare(0, 5, "help:") != 0 &&
         text.compare(0, 9, "category:") != 0 &&
         text.compare(0, 7, "portal:") != 0 &&
         text.compare(0, 5, "book:") != 0 &&
         text.compare(0, 6, "draft:") != 0 &&
-        text.compare(0, 10, "timedText:") != 0 &&
+        text.compare(0, 10, "timedtext:") != 0 &&
         text.compare(0, 7, "module:") != 0 &&
-        text.compare(0, 6, "topic:") != 0 &&
-
-        text.compare(0, 7, "list of") != 0 &&
-
-        text.indexOf("(disambiguation)") == -1;
-}
-
-bool SAX2Handler::interLanguage(icu::UnicodeString link) const {
-    for(auto const& language : SAX2Handler::languages) {
-        if(link.compare(0, 3, language) == 0) {
-            return true;
-        }
-    }
-
-    return false;
+        text.compare(0, 6, "topic:") != 0;
 }
--- graph-extractor/sax2-handler.hpp
+++ graph-extractor/sax2-handler.hpp
@@ -9,36 +9,33 @@
 #include <unicode/unistr.h>
 
 class SAX2Handler : public xercesc::DefaultHandler {
-public:
-    SAX2Handler();
+    public:
+        SAX2Handler();
 
-    void startElement(
-        const XMLCh* const uri,
-        const XMLCh* const localname,
-        const XMLCh* const qname,
-        const xercesc::Attributes& attrs);
+        void startElement(
+            const XMLCh* const uri,
+            const XMLCh* const localname,
+            const XMLCh* const qname,
+            const xercesc::Attributes& attrs);
 
-    void endElement(
-        const XMLCh* const uri,
-        const XMLCh* const localname,
-        const XMLCh* const qname);
+        void endElement(
+            const XMLCh* const uri,
+            const XMLCh* const localname,
+            const XMLCh* const qname);
 
-    void characters(const XMLCh* const chars,
-        const XMLSize_t length);
+        void characters(const XMLCh* const chars,
+            const XMLSize_t length);
 
-    bool allowedTitle(icu::UnicodeString title) const;
-    bool allowedLink(icu::UnicodeString link) const;
-    bool allowed(icu::UnicodeString text) const;
-    bool interLanguage(icu::UnicodeString link) const;
+        bool allowed(icu::UnicodeString text) const;
 
-private:
-    // a stack to track where in the XML document we are since we're using SAX2 parsing
-    std::stack<icu::UnicodeString> state;
+    private:
+        // a stack to track where in the XML document we are since we're using SAX2 parsing
+        std::stack<icu::UnicodeString> state;
 
-    // per-page data, reset after every page element is parsed
-    icu::UnicodeString title;
-    icu::UnicodeString content;
-    bool redirect;
+        // per-page data, reset after every page element is parsed
+        icu::UnicodeString title;
+        icu::UnicodeString content;
+        bool redirect;
 
-    static const char* languages[];
+        static const char* languages[];
 };
